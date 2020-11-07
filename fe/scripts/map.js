f5ad56adfaca27d016952fb264e60e2773c66d28
Marko Prelevikj <pr3mar@users.noreply.github.com>
2020-11-07 20:45:27 +0100

    Added the markers

--- fe/scripts/map.js
+++ fe/scripts/map.js
@@ -1,22 +1,39 @@
-const baseUrl = 'http://localhost:5000/api/spills'
+const serverBaseUrl = 'http://localhost:5000/api/spills'
 
-function load_spills() {
-  axios.get(baseUrl)
+const spillMarkers = L.layerGroup();
+
+const mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+  '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const mbUrl = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
+
+const grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+  streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
+
+const spillMap = L.map('oilMap', {
+  center: [-21., 56.],
+  zoom: 5,
+  layers: [grayscale, spillMarkers]
+});
+
+(() => {
+  axios.get(serverBaseUrl)
     .then(data => {
-      console.log(data);
-      // TODO: add the markers to the map
+      for (record of data.data) {
+        const coords = record.Coords.split(';');
+        L.marker(coords).bindPopup(record.Spill).addTo(spillMarkers);
+      }
     })
-    .catch(err => console.log(err))
-}
+    .catch(err => console.log(err));
+})();
 
-var mymap = L.map('oilMap').setView([-21., 56.], 5);
+const baseLayers = {
+  "Streets": streets,
+  "Grayscale": grayscale
+};
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1
-}).addTo(mymap);
+const overlays = {
+  "Cities": spillMarkers
+};
+
+L.control.layers(baseLayers, overlays).addTo(spillMap);

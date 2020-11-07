a7f357c55e24d422dc3bac0a49a63a1a98ac55ed
Marko Prelevikj <pr3mar@users.noreply.github.com>
2020-11-07 23:36:57 +0100

    Added the tabs

--- fe/scripts/map.js
+++ fe/scripts/map.js
@@ -21,7 +21,41 @@ const spillMap = L.map('oilMap', {
     .then(data => {
       for (record of data.data) {
         const coords = record.Coords.split(';');
-        L.marker(coords).bindPopup(record.Spill).addTo(spillMarkers);
+        console.log(record);
+        L.marker(coords).bindPopup( '<div tabs>' +
+
+          '<div class="tab" id="tab-1">' +
+          '<div class="content">' +
+          '<b>Spill Info</b>' +
+          '<table>' +
+          '<tr><td>Spill</td><td>' + record.Spill + '</td></tr>' +
+          '<tr><td>Date</td><td>' + record.Dates + '</td></tr>' +
+          '<tr><td>Location</td><td>' + record.Location + '</td></tr>' +
+          '<tr><td>Min tonnes</td><td>' + (record.MinTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
+          '<tr><td>Max tonnes</td><td>' + (record.MaxTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
+          '<tr><td>Owner</td><td>' + record.Owner + '</td></tr>' +
+          '</table>' +
+          '</div>' +
+          '</div>' +
+
+          '<div class="tab" id="tab-2">' +
+          '<div class="content">' +
+          '<b>History</b>' +
+          '</div>' +
+          '</div>' +
+
+          '<div class="tab" id="tab-3">' +
+          '<div class="content">' +
+          '<b>Public reaction</b>' +
+          '</div>' +
+          '</div>' +
+
+          '<ul class="tabs-link">' +
+          '<li class="tab-link"> <a href="#tab-1"><span>Spill Info</span></a></li>' +
+          '<li class="tab-link"> <a href="#tab-2"><span>History</span></a></li>' +
+          '<li class="tab-link"> <a href="#tab-3"><span>Public Reaction</span></a></li>' +
+          '</ul>' +
+          '</div>').addTo(spillMarkers);
       }
     })
     .catch(err => console.log(err));

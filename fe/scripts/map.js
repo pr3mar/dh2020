60e9f86cb0a3bb98e59fb1ca5a99517efbcd6856
ghajduk3 <hajdukovicgojko3@gmail.com>
2020-11-08 09:22:12 +0100

    FE

--- fe/scripts/map.js
+++ fe/scripts/map.js
@@ -22,40 +22,48 @@ const spillMap = L.map('oilMap', {
       for (record of data.data) {
         const coords = record.Coords.split(';');
         console.log(record);
-        L.marker(coords).bindPopup( '<div tabs>' +
+        L.marker(coords).bindPopup(content
+            // '<div data-tabs>' +
 
-          '<div class="tab" id="tab-1">' +
-          '<div class="content">' +
-          '<b>Spill Info</b>' +
-          '<table>' +
-          '<tr><td>Spill</td><td>' + record.Spill + '</td></tr>' +
-          '<tr><td>Date</td><td>' + record.Dates + '</td></tr>' +
-          '<tr><td>Location</td><td>' + record.Location + '</td></tr>' +
-          '<tr><td>Min tonnes</td><td>' + (record.MinTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
-          '<tr><td>Max tonnes</td><td>' + (record.MaxTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
-          '<tr><td>Owner</td><td>' + record.Owner + '</td></tr>' +
-          '</table>' +
-          '</div>' +
-          '</div>' +
-
-          '<div class="tab" id="tab-2">' +
-          '<div class="content">' +
-          '<b>History</b>' +
-          '</div>' +
-          '</div>' +
-
-          '<div class="tab" id="tab-3">' +
-          '<div class="content">' +
-          '<b>Public reaction</b>' +
-          '</div>' +
-          '</div>' +
-
-          '<ul class="tabs-link">' +
-          '<li class="tab-link"> <a href="#tab-1"><span>Spill Info</span></a></li>' +
-          '<li class="tab-link"> <a href="#tab-2"><span>History</span></a></li>' +
-          '<li class="tab-link"> <a href="#tab-3"><span>Public Reaction</span></a></li>' +
-          '</ul>' +
-          '</div>').addTo(spillMarkers);
+  //         '<div class="tab" id="tab-1">' +
+  //         '<div class="content">' +
+  //         '<b>Spill Info</b>' +
+  //         '<table>' +
+  //         '<tr><td>Spill</td><td>' + record.Spill + '</td></tr>' +
+  //         '<tr><td>Date</td><td>' + record.Dates + '</td></tr>' +
+  //         '<tr><td>Location</td><td>' + record.Location + '</td></tr>' +
+  //         '<tr><td>Min tonnes</td><td>' + (record.MinTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
+  //         '<tr><td>Max tonnes</td><td>' + (record.MaxTonnes ? record.MinTonnes : 'N/A') + '</td></tr>' +
+  //         '<tr><td>Owner</td><td>' + record.Owner + '</td></tr>' +
+  //         '</table>' +
+  //         '</div>' +
+  //         '</div>' +
+  //
+  //         '<div class="tab" id="tab-2">' +
+  //         '<div class="content">' +
+  //         '<b>History</b>' +
+  //         '</div>' +
+  //         '</div>' +
+  //
+  //         '<div class="tab" id="tab-3">' +
+  //         '<div class="content">' +
+  //         '<b>Public reaction</b>' +
+  //         '</div>' +
+  //         '</div>' +
+  //
+  //          ' <div class="topnav">' +
+  //           '<a class="tab-link" href="#tab-1">Spill info</a>' +
+  //           '<a class="tab-link" href="#tab-2">History</a>' +
+  //           '<a class="tab-link" href="#contact">Contact</a>' +
+  // // <a href="#about">About</a>
+  //           '</div>'
+          // '<ul data-tabs>' +
+          // '<li class="tab-link"> <a href="#tab-1"><span>Spill Info</span></a></li>' +
+          // '<li class="tab-link"> <a href="#tab-2"><span>History</span></a></li>' +
+          // '<li class="tab-link"> <a href="#tab-3"><span>Public Reaction</span></a></li>' +
+          // '</ul>'
+          // '</div>'
+        ).addTo(spillMarkers)
       }
     })
     .catch(err => console.log(err));
@@ -71,3 +79,30 @@ const overlays = {
 };
 
 L.control.layers(baseLayers, overlays).addTo(spillMap);
+var content = '<div class="tabs">' +
+
+            '<div class="tab" id="tab-1">' +
+            '<div class="content">' +
+            '<b>Tab 1 content</b>' +
+            '</div>' +
+            '</div>' +
+
+            '<div class="tab" id="tab-2">' +
+            '<div class="content">' +
+            '<b>Tab 2 content</b>' +
+            '</div>' +
+            '</div>' +
+
+            '<div class="tab" id="tab-3">' +
+            '<div class="content">' +
+            '<b>Tab 3 content</b>' +
+            '</div>' +
+            '</div>' +
+
+            '<ul class="tabs-link">' +
+            '<li class="tab-link"> <a href="#tab-1"><span>Spill Info</span></a></li>' +
+            '<li class="tab-link"> <a href="#tab-2"><span>Tab 2</span></a></li>' +
+            '<li class="tab-link"> <a href="#tab-3"><span>Tab 3</span></a></li>' +
+            '</ul>' +
+        '</div>';
+var tabs = new Tabby('[data-tabs]');
